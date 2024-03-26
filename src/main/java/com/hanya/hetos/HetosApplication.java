ec74cea23b21e060ec8e3b601af34e32d1f0fe0c
terius <tingtangting9398@gmail.com>
2024-03-26 22:56:35 +0900

    init

---
+++ src/main/java/com/hanya/hetos/HetosApplication.java
@@ -0,0 +1,13 @@
+package com.hanya.hetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HetosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HetosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanya/hetos/HetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanya.hetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HetosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
